1dca2f9850dbb957a21bc4c831fcde4f0731c19a
lucianilh <oluishenriqueluciani@gmail.com>
2021-09-15 23:11:35 -0300

    Correção na parte de saque

--- models/conta.py
+++ models/conta.py
@@ -72,7 +72,7 @@ class Conta:
                 self.limite = self.limite + restante
                 self.saldo = 0
                 self.saldo_total = self._calcula_saldo_total
-                print('Saque efetuado com sucesso.')
+            print('Saque efetuado com sucesso.')
         else:
             print('Saque não realizado. Tente novamente.')
 
